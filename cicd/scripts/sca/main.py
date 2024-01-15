7450b21d99a6ce041623895fafa0738be23e98ed
whenao31 <whenao86@gmail.com>
2024-01-14 20:18:38 -0500

    add function to carry out POST request to SCA service API

--- cicd/scripts/sca/main.py
+++ cicd/scripts/sca/main.py
@@ -1,16 +1,18 @@
+import json
 import os
-import subprocess
-import sys, getopt
+import sys
 from util import check_docker, get_sca_opt, parse_markdowntable_to_json
+from http_request import post_sca_result
 
 
-def main(argv=None):
+def main(argv):
     
     # rm_previous_file_cmd = "[ ! -e sca_result.md ] || rm sca_result.md"
     # print(os.system(rm_previous_file_cmd))
 
-    lockfile = get_sca_opt(argv)
-
+    # Obtain all the args from command line
+    lockfile, ci_exec_id, git_branch, git_user, sca_api_url  = get_sca_opt(argv)
+    
     docker_sca_cmd = f'docker run -it -v ${{PWD}}:/src ghcr.io/google/osv-scanner --format markdown -L /src/{lockfile} > sca_result.md'
     os.system(docker_sca_cmd)
 
@@ -24,11 +26,17 @@ def main(argv=None):
         print('\n*** SCA Result Warn ***')
         print(head, '\n', tail)
     
-    print(parse_markdowntable_to_json('sca_result.md'))    
+    # Parse data to send to Vulnerability Management Platform(VMP)
+    data  = parse_markdowntable_to_json('sca_result.md', execution_id=ci_exec_id,
+                                       git_branch=git_branch, git_user=git_user
+            )
+    
+    # POST Request of the SCA results to the SCA service at VMP
+    post_sca_result(sca_api_url, data)
+    
     
 
 if __name__ == '__main__':
 
     check_docker()
     main(sys.argv[1:])
-    print('executed ;)')
--- cicd/scripts/sca/util.py
+++ cicd/scripts/sca/util.py
@@ -29,32 +29,47 @@ def get_sca_opt(argv):
     Returns: 
         lockfile_path
     '''
-    lockfile_path = ''
+    lockfile_path   = ''
+    ci_exec_id      = ''
+    git_branch      = ''
+    git_user        = ''
+    sca_api_url     = ''
 
     try:
-        opts, args = getopt.getopt(argv, "hL:", ["lockfile=",])
+        long_options = ["lockfile=", "ci-exec-id=", "git-branch", "git-user", "sca-api-url"]
+        opts, args = getopt.getopt(argv, "hL:i:b:u:a:", long_options)
     except getopt.GetoptError:
-        print("sca.py -L [lockfile/path]")
+        print("sca.py -L [lockfile/path] -i [ci-execution-id] -b [git-branch] -u [git-user] -a [sca-api-url]")
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print ('sca.py -L [lockfile/path]')
+            print ('sca.py -L [lockfile/path] -i [ci-execution-id] -b [git-branch] -u [git-user] -a [sca-api-url]')
             sys.exit()
         elif opt in ('-L', "--lockfile"):
             lockfile_path = arg
+        elif opt in ('-i', "--ci-exec-id"):
+            ci_exec_id = arg
+        elif opt in ('-b', "--git-branch"):
+            git_branch = arg
+        elif opt in ('-u', "--git-user"):
+            git_user = arg
+        elif opt in ('-a', "--sca-api-url"):
+            sca_api_url = arg
     
-    return lockfile_path
+    return lockfile_path, ci_exec_id, git_branch, git_user, sca_api_url
 
 
-def parse_markdowntable_to_json(filename):
+def parse_markdowntable_to_json(filename, **kwargs):
     """Parses text separated by '|' within a file into a JSON object.
 
     Args:
-    filename: name of the file containing the text to parse.
-
+        filename: name of the file containing the text to parse.
+        kwarg.execution_id: Hash from the cicd execution
+        kwarg.git_branch: Git branch of the analized lockfile repository
+        kwarg.git_user: User who executed the cicd pipeline
     Returns:
-    A JSON object representing the parsed data.
+        A JSON object representing the parsed data.
     """
 
     with open(filename, "r") as file:
@@ -68,5 +83,9 @@ def parse_markdowntable_to_json(filename):
         fields = [field.strip() for field in line[1:len(line) - 1].split("|")]
         entry = dict(zip(headers, fields))
         data.append(entry)
+    
+    data_dict = {'vulnerabilities': data}
+    for key, value in kwargs.items():
+        data_dict[key] = value
 
-    return json.dumps(data, indent=4)
+    return json.dumps(data_dict, indent=4)
